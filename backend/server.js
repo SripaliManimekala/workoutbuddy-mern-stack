35b57a5bb729bdd09c20f4a58a6dffdec46f5eba
SripaliManimekala <sripalimanimekala@gmail.com>
2023-12-04 07:55:33 +0530

    reacting to requests

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 //ENTRY FILE FOR THE BACKEND APPLICATION
+require('dotenv').config()
+
 const express = require('express')
 
 ///start of the express app
 const app = express()
 
-//listen for requests
-app.listen(4000, () => {
-    console.log('listening on port 4000')
+//reacting to requests
+//routes
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app'})
+})
+
+//listen for requests(here process is a global object)
+app.listen(process.env.PORT, () => {
+    console.log('listening on port' , process.env.PORT)
 })
