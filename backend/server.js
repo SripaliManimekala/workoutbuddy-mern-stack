4d9abc51acd97d85fcd60f281a1bc80c640cc32b
SripaliManimekala <sripalimanimekala@gmail.com>
2023-12-04 12:11:45 +0530

    installing mongoose to help with struing bcz mongodb alone doesnt support schemas

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,14 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 ///start of the express app
 const app = express()
 
 //middleware is the code that executes between us getting a request on the server and us sending a response
-app.use(express.json())//looks if there is a body to the request
+app.use(express.json())//looks if there is a body to the request if it has it atachwes it to the req object
 //this function will fire for every request that comes in
 app.use((req, res, next) => {
     console.log("Middleware is working")
