85cc2378a1d9db11d71e0290559f5ba6ce361f4c
SripaliManimekala <sripalimanimekala@gmail.com>
2023-12-04 07:04:36 +0530

    listen for requests

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,8 @@ const express = require('express')
 
 ///start of the express app
 const app = express()
+
+//listen for requests
+app.listen(4000, () => {
+    console.log('listening on port 4000')
+})
