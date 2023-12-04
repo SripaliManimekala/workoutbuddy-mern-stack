d36da8d2299d3ae15762a4d111c41437b4ea92f2
SripaliManimekala <sripalimanimekala@gmail.com>
2023-12-04 14:53:19 +0530

    connect to the database and and listen to requests

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,15 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/workouts', workoutRoutes)
 
-//listen for requests(here process is a global object)
-app.listen(process.env.PORT, () => {
-    console.log('listening on port' , process.env.PORT)
-})
+//use mongoose object to connect to the database
+mongoose.connect(process.env.MONGO_URI)//this is async in nature,takes a lil bit time to do, so it returns a promise, so we tack on it using .then method to fire a function when its complete
+    .then(() =>{
+        //listen for requests(here process is a global object) omce we connected to the db
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port' , process.env.PORT)
+        })
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
+
