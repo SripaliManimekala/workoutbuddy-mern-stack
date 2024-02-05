c3e902b98c707c837d8c1c88308af2afbf4798dd
SripaliManimekala <sripalimanimekala@gmail.com>
2024-02-05 19:58:25 +0530

    add controller functions of login and sign up to request handlers

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,14 @@
 const express = require('express')
 
+//controller functions
+const {  signupUser, loginUser } = require('../controllers/userController')
+
 const router = express.Router()//instance of the express router
 
 //we need a login route
-router.post('/login', () => {})
+router.post('/login', loginUser)
 
 //and a sign up route
-router.post('/signup', () => {})
+router.post('/signup', signupUser)
 
 module.exports =  router
