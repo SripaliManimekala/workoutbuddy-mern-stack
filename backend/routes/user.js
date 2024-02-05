f18d8d4de398f3de4b0e09b61b41a7db85f3e68c
SripaliManimekala <sripalimanimekala@gmail.com>
2024-02-05 19:21:15 +0530

    created the user.js file inside routes folder to handle the user login and signup routes

---
+++ backend/routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()//instance of the express router
+
+//we need a login route
+router.post('/login', () => {})
+
+//and a sign up route
+router.post('/signup', () => {})
+
+module.exports =  router
